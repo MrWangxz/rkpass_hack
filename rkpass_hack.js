1d34246448e8b0528b0c4e897bb80d342e1d7d3f
Sincky <30948192+Sincky@users.noreply.github.com>
2019-02-22 21:48:08 +0800

    Update rkpass_hack.js

--- rkpass_hack.js
+++ rkpass_hack.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         rkpass_hack
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       Sincky
 // @match        http://www.rkpass.cn/tk_timu/*xuanze.html
